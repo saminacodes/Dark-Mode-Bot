eb3b3e41e28bac9f868e145a3c50f2bfe2340216
Kelly Copley <copleykj@gmail.com>
2021-08-20 10:04:12 -0400

    add example ping command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { registerGuildCommands } from './utils/commands.js';
 import { socketClient } from './discord-client/index.js';
+import './modules/index.js';
 
 socketClient.on('ready', async () => {
   console.log('client connected and ready');
